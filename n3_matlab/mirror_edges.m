73a71b43027855d8c5ec2a9c4a7b6ae61b9e28f5
Pekala <pekalmj1@pekalmj1-ml1.jhuapl.edu>
2015-05-20 15:35:46 -0400

    changed how is implemented.

--- n3_matlab/mirror_edges.m
+++ n3_matlab/mirror_edges.m
@@ -1,41 +1,30 @@
 function Xm = mirror_edges(X, nPixels)
-%  MIRROR_EDGES
+%  MIRROR_EDGES - Reflects border of nPixels around X.
+%
+% May 2015, mjp
 
 flip_corner = @(M) fliplr(flipud(M));
     
 [m,n,p] = size(X);
-Xm = zeros(m+2*nPixels, n+2*nPixels, p);
+Xm = NaN*ones(m+2*nPixels, n+2*nPixels, p);
 
-Xm(nPixels:m+nPixels-1, nPixels:n+nPixels-1, :) = X;
+% image interior
+Xm(nPixels+1:m+nPixels, nPixels:n+nPixels-1, :) = X;
 
-% TODO: don't need to do corners explicitly - will fall out properly if mirror borders.
+% Note: don't need to handle corners explicitly (e.g. as was done
+% in Python code); careful mirroring of edges will take care of
+% this natrually.
 
 for ii = 1:p
-    Xi = X(:,:,ii);
-    
-    % top left corner
-    Xm(1:nPixels, 1:nPixels, ii) = flip_corner(Xi(1:nPixels, 1:nPixels));
+    % do left, right edges
+    rows = (nPixels+1):size(Xm,1)-nPixels;
+    Xm(rows, 1:nPixels, ii) = fliplr(X(:,1:nPixels, ii));
+    Xm(rows, end-nPixels:end, ii) = fliplr(X(:,end-nPixels:end, ii));
 
-    % top right corner
-    Xm(1:nPixels, n+nPixels+1:end, ii) = flip_corner(Xi(1:nPixels, n-nPixels+1:end));
-
-    % bottom left corner
-    Xm(m+nPixels+1:end, 1:nPixels) = flip_corner(Xi(m-nPixels+1:end, 1:nPixels));
-    
-    % bottom right corner
-    Xm(m+nPixels+1:end, n+nPixels+1:end, ii) = flip_corner(Xi(m-nPixels+1:end, n-nPixels+1:end));
-    
-    % top border
-    Xm(1:nPixels, nPixels+1:n+nPixels, ii) = flipud(Xi(1:nPixels, :));
-
-    % bottom border
-    Xm(m+nPixels+1:end, nPixels+1:n+nPixels, ii) = flipud(Xi(m-nPixels+1:end, :));
-
-    % left border
-    Xm(nPixels+1:m+nPixels, 1:nPixels, ii) = fliplr(Xi(:, 1:nPixels));
-
-    % right border
-    Xm(nPixels+1:m+nPixels, n+nPixels+1:end, ii) = fliplr(Xi(:, n-nPixels+1:end));
+    % top, bottom edges.
+    % Grab these from Xm (vs X) so we get properly reflected corners
+    Xm(1:nPixels, :, ii) = flipud(Xm(nPixels+1:2*nPixels, :, ii));    
+    Xm(end-nPixels:end, :, ii) = flipud(Xm(end-2*nPixels:end-nPixels, :, ii)); 
 end
 
-
+assert(all(all(isfinite(Xm))));
