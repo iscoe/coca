f9eb882b961b1c139db89f42d1cd410ccb4a2402
Pekala <pekalmj1@pekalmj1-ml1.jhuapl.edu>
2015-05-21 09:21:24 -0400

    added a few comments.

--- Tools/N3_S2_Matlab/mirror_edges.m
+++ Tools/N3_S2_Matlab/mirror_edges.m
@@ -1,10 +1,20 @@
 function Xm = mirror_edges(X, nPixels)
-%  MIRROR_EDGES - Reflects border of nPixels around X.
+% MIRROR_EDGES - Reflects border of nPixels around X.
+%
+%    The original Python Caffe code expands the original image by
+%    reflecting the border (as per Ciresan's original paper).
+%    Artifically expanding the image in this fashion makes the 'valid'
+%    portion of a convolution the same size as the original image.
+%
+%    PARAMETERS:
+%      X := a data volume (3d tensor)
+%      nPixels := the size of the border to add via mirroring
+%
+%    RETURNS:
+%      Xm := a copy of X with mirrored edges
 %
 % May 2015, mjp
 
-flip_corner = @(M) fliplr(flipud(M));
-    
 [m,n,p] = size(X);
 Xm = NaN*ones(m+2*nPixels, n+2*nPixels, p);
 
@@ -16,15 +26,16 @@ Xm(nPixels+1:m+nPixels, nPixels:n+nPixels-1, :) = X;
 % this natrually.
 
 for ii = 1:p
-    % do left, right edges
+    % do left and right edges
     rows = (nPixels+1):size(Xm,1)-nPixels;
     Xm(rows, 1:nPixels, ii) = fliplr(X(:,1:nPixels, ii));
     Xm(rows, end-nPixels:end, ii) = fliplr(X(:,end-nPixels:end, ii));
 
-    % top, bottom edges.
-    % Grab these from Xm (vs X) so we get properly reflected corners
+    % Now, top and bottom edges.
+    % Grab these from Xm (vs X) so we get properly reflected corners.
     Xm(1:nPixels, :, ii) = flipud(Xm(nPixels+1:2*nPixels, :, ii));    
     Xm(end-nPixels:end, :, ii) = flipud(Xm(end-2*nPixels:end-nPixels, :, ii)); 
 end
 
+% make sure we didn't miss anything in the operations above
 assert(all(all(isfinite(Xm))));
