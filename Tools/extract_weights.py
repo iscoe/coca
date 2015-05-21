7859826e59fd1d294b545d526daafb66830dee7c
Pekala <pekalmj1@pekalmj1-ml1.jhuapl.edu>
2015-05-21 09:11:21 -0400

    updated top level comments

--- Tools/extract_weights.py
+++ Tools/extract_weights.py
@@ -1,8 +1,16 @@
-# Extracts weights from a Caffe model file (saves weights from each
-# layer to separate files).
+# Extracts weights from a Caffe model file.
+#
+#  This script saves the weights from each Caffe DNN layer to a
+#  separate file.  These weight files can be analyzed individually or
+#  used to replicate the action of the CNN in another environment
+#  (e.g. Matlab).
+#
+#  This script requires the PyCaffe interface, a model file (created
+#  during a Caffe training run) and the corresponding Caffe network
+#  prototxt file.
 #
 #  Example:
-#    PYTHONPATH=~/Apps/caffe/python python coca/extract_weights.py -m iter_180000.caffemodel  -n n3-net.prototxt
+#    PYTHONPATH=~/Apps/caffe/python python coca/Tools/extract_weights.py -m iter_180000.caffemodel  -n n3-net.prototxt
 #
 # May 2015, Mike Pekala
 
@@ -16,12 +24,8 @@ import scipy.io
 
 
 def get_args():
- 
     parser = argparse.ArgumentParser('Extract CNN weights')
 
-    #----------------------------------------
-    # Parameters for defining the neural network
-    #----------------------------------------
     parser.add_argument('-m', dest='modelFile', type=str, required=True,
                         help='Caffe model weights file to parse')
     parser.add_argument('-n', dest='netFile', type=str, required=True,
@@ -39,7 +43,8 @@ if __name__ == "__main__":
     net = caffe.Net(args.netFile, args.modelFile)
     for name, blobs in net.params.iteritems():
         for ii in range(len(blobs)):
-            # assume here index 0 are the weights and 1 is the bias
+            # Assume here index 0 are the weights and 1 is the bias.
+            # This seems to be the case in Caffe.
             if ii == 0:
                 name2 = name + "Weight"
             elif ii == 1:
