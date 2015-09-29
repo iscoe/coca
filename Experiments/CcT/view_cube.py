554675e1bca9715da4e7211630ff542da6ebfd32
mike pekala <mpekala@umd.edu>
2015-09-29 15:01:15 -0400

    minor cosmetic chagnes

--- Experiments/CcT/view_cube.py
+++ Experiments/CcT/view_cube.py
@@ -8,8 +8,9 @@ inFile='./lenet/Yhat_train-volume.tif.npy'
 
 Yhat = np.load(inFile)
 
-sliceId = 1;
+sliceId = 0;
+classId = 1;
 
-plt.imshow(Yhat[1,sliceId,:,:], cmap='bone')
+plt.imshow(Yhat[classId,sliceId,:,:], cmap='bone')
 plt.colorbar()
 plt.show()
