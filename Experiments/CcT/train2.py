5a1ee3437edc9229872476c7c057c92a69e322c1
mike pekala <mpekala@umd.edu>
2015-10-01 13:50:33 -0400

    updated

--- Experiments/CcT/train2.py
+++ Experiments/CcT/train2.py
@@ -1,4 +1,13 @@
 """ A refactored version of ../../train.py
+
+** NOTE **
+This code assumes your network has the following layer types/tops:
+
+    top name |  Layer type
+    ---------+-----------------
+      loss   |  SoftmaxWithLoss
+      acc    |  Accuracy
+      prob   |  Softmax
 """
 
 __author__ = "Mike Pekala"
@@ -58,12 +67,14 @@ def _get_args():
 
     
     parser.add_argument('--omit-labels', dest='omitLabels', 
-		    type=str, default='(-1,)', 
+		    type=str, default='(-2, -1,)', 
 		    help='(optional) list of labels to omit')
-    
     parser.add_argument('--rotate-data', dest='rotateData', 
 		    type=int, default=0, 
 		    help='(optional) 1 := apply arbitrary rotations')
+    parser.add_argument('--only-slices', dest='onlySlices', 
+		    type=str, default='', 
+		    help='(optional) limit experiment to a subset of slices')
 
     return parser.parse_args()
 
@@ -191,7 +202,7 @@ def train_one_epoch(solver, X, Y,
         #----------------------------------------
         _tmp = time.time()
         solver.net.set_input_arrays(Xi, yi)
-        rv = solver.net.forward()
+        out = solver.net.forward()
         solver.net.backward()
 
         # SGD with momentum
@@ -203,12 +214,16 @@ def train_one_epoch(solver, X, Y,
                 blob.data[...] += Vnext
                 trainInfo.V[key] = Vnext
 
+        # (try to) extract some useful info from the net
+        loss = out.get('loss', None)
+        acc = out.get('acc', None)  # accuracy is not required...
+
         # update run statistics
         trainInfo.cnnTime += time.time() - _tmp
         trainInfo.iter += 1
         trainInfo.netTime += (time.time() - tic)
         tic = time.time()
-        loss = np.squeeze(rv['loss'])
+
 
         #----------------------------------------
         # Some events occur on regular intervals.
@@ -222,9 +237,12 @@ def train_one_epoch(solver, X, Y,
             trainInfo.alpha *= trainInfo.gamma
 
         if (trainInfo.iter % trainInfo.param.display) == 1: 
-            print "[train]: completed iteration %d of %d;" % (trainInfo.iter, trainInfo.param.max_iter)
-            print "[train]:     loss=%0.2f, %0.2f min elapsed (%0.2f CNN min)" % (loss, trainInfo.netTime/60., trainInfo.cnnTime/60.)
-            print "[train]:     epoch pct. complete=%0.2f" % epochPct
+            print "[train]: completed iteration %d of %d (epoch complete=%0.2f%%;" % (trainInfo.iter, trainInfo.param.max_iter, 100.*epochPct)
+            print "[train]:     %0.2f min elapsed (%0.2f CNN min)" % (trainInfo.netTime/60., trainInfo.cnnTime/60.)
+            if loss: 
+                print "[train]:     loss=%0.2f" % loss
+            if acc: 
+                print "[train]:     accuracy (train volume)=%0.2f" % acc
             sys.stdout.flush()
  
         if trainInfo.iter >= trainInfo.param.max_iter:
@@ -244,187 +262,35 @@ def train_one_epoch(solver, X, Y,
 def predict(net, X, Y, batchDim, outDir, omitLabels=[]):
     """Generates a prediction for a volume.
     """
-    assert(batchDim[2] == batchDim[3])     # tiles must be square
 
-    # Some variables and storage that we'll use in the loop below
+    # Pre-allocate some variables & storate.
     #
     tileRadius = int(batchDim[2]/2)
     Xi = np.zeros(batchDim, dtype=np.float32)
     yi = np.zeros((batchDim[0],), dtype=np.float32)
-    yMax = np.max(Y).astype(np.int32)                
-    
-    losses = np.zeros((solverParam.max_iter,)) 
-    acc = np.zeros((solverParam.max_iter,))
-    currIter = 0
-    currEpoch = 0
+    yMax = np.max(Y).astype(np.int32)
 
-    # SGD parameters.  SGD with momentum is of the form:
-    #
-    #    V_{t+1} = \mu V_t - \alpha \nablaL(W_t)
-    #    W_{t+1} = W_t + V_{t+1}
-    #
-    # where W are the weights and V the previous update.
-    # Ref: http://caffe.berkeleyvision.org/tutorial/solver.html
-    #
-    alpha = solverParam.base_lr            # alpha := learning rate
-    mu = solverParam.momentum              # mu := momentum
-    gamma = solverParam.gamma              # gamma := step factor
-    isModeStep = (solverParam.lr_policy == u'step')
-    isTypeSGD = (solverParam.solver_type == solverParam.SolverType.Value('SGD'))
-    Vall = {}                              # stores previous SGD steps (for all layers)
+    Mask = np.ones(Y.shape, dtype=bool)
+    for yIgnore in omitLabels:
+        Mask[Yvalid==yIgnore] = False
 
-    if not (isModeStep and isTypeSGD):
-        raise RuntimeError('Sorry - only support SGD "step" mode at the present')
- 
-    # TODO: weight decay
-    # TODO: layer-specific weights
- 
-    cnnTime = 0.0                          # time spent doing core CNN operations
-    tic = time.time()
-    
-    while currIter < solverParam.max_iter:
+    print "[train]:  Evaluating %0.2f%% of cube" % (100.0*np.sum(Mask)/numel(Mask)) 
+    it = emlib.interior_pixel_generator(Y, tileRadius, batchDim[0], mask=Mask)
+    for Idx, epochPct in it: 
+        # Extract subtiles from validation data set 
+        for jj in range(Idx.shape[0]): 
+            a = Idx[jj,1] - tileRadius 
+            b = Idx[jj,1] + tileRadius + 1 
+            c = Idx[jj,2] - tileRadius 
+            d = Idx[jj,2] + tileRadius + 1 
+            Xi[jj, 0, :, :] = Xvalid[ Idx[jj,0], a:b, c:d ]
+            yi[jj] = Yvalid[ Idx[jj,0], Idx[jj,1], Idx[jj,2] ] 
 
-        #--------------------------------------------------
-        # Each generator provides a single epoch's worth of data.
-        # However, Caffe doesn't really recognize the notion of an epoch; instead,
-        # they specify a number of training "iterations" (mini-batch evaluations, I assume).
-        # So the inner loop below is for a single epoch, which we may terminate
-        # early if the max # of iterations is reached.
-        #--------------------------------------------------
-        currEpoch += 1
-        it = emlib.stratified_interior_pixel_generator(Y, tileRadius, batchDim[0], omitLabels=omitLabels)
-        for Idx, epochPct in it:
-            # Map the indices Idx -> tiles Xi and labels yi
-            # 
-            # Note: if Idx.shape[0] < batchDim[0] (last iteration of an epoch) a few examples
-            # from the previous minibatch will be "recycled" here. This is intentional
-            # (to keep batch sizes consistent even if data set size is not a multiple
-            #  of the minibatch size).
-            #
-            for jj in range(Idx.shape[0]):
-                yi[jj] = Y[ Idx[jj,0], Idx[jj,1], Idx[jj,2] ]
-                a = Idx[jj,1] - tileRadius
-                b = Idx[jj,1] + tileRadius + 1
-                c = Idx[jj,2] - tileRadius
-                d = Idx[jj,2] + tileRadius + 1
-                Xi[jj, 0, :, :] = X[ Idx[jj,0], a:b, c:d ]
-
-            # label-preserving data transformation (synthetic data generation)
-            if syn_func is not None:
-                Xi = syn_func(Xi)
-
-            #----------------------------------------
-            # one forward/backward pass and update weights
-            # (SGD with momentum term)
-            #----------------------------------------
-            _tmp = time.time()
-            solver.net.set_input_arrays(Xi, yi)
-            rv = solver.net.forward()
-            solver.net.backward()
-
-            for lIdx, layer in enumerate(solver.net.layers):
-                for bIdx, blob in enumerate(layer.blobs):
-                    key = (lIdx, bIdx)
-                    V = Vall.get(key, 0.0)
-                    Vnext = mu*V - alpha * blob.diff
-                    blob.data[...] += Vnext
-                    Vall[key] = Vnext
-            cnnTime += time.time() - _tmp
-                    
-            # update running list of losses with the loss from this mini batch
-            losses[currIter] = np.squeeze(rv['loss'])
-            acc[currIter] = np.squeeze(rv['accuracy'])
-            currIter += 1
-
-            #----------------------------------------
-            # Some events occur on mini-batch intervals.
-            # Deal with those now.
-            #----------------------------------------
-            if (currIter % solverParam.snapshot) == 0:
-                fn = os.path.join(outDir, 'iter_%06d.caffemodel' % (currIter))
-                solver.net.save(str(fn))
-
-            if isModeStep and ((currIter % solverParam.stepsize) == 0):
-                alpha *= gamma
-
-            if (currIter % solverParam.display) == 1:
-                elapsed = (time.time() - tic)/60.
-                print "[train]: completed iteration %d (of %d; %0.2f min elapsed; %0.2f CNN min)" % (currIter, solverParam.max_iter, elapsed, cnnTime/60.)
-                print "[train]:    epoch: %d (%0.2f), loss: %0.3f, acc: %0.3f, learn rate: %0.3e" % (currEpoch, 100*epochPct, np.mean(losses[max(0,currIter-10):currIter]), np.mean(acc[max(0,currIter-10):currIter]), alpha)
-                sys.stdout.flush()
- 
-            if currIter >= solverParam.max_iter:
-                break  # in case we hit max_iter on a non-epoch boundary
-
-                
-        #--------------------------------------------------
-        # After each training epoch is complete, if we have validation
-        # data, evaluate it.
-        # Note: this only requires forward passes through the network
-        #--------------------------------------------------
-        if (Xvalid is not None) and (Xvalid.size != 0) and (Yvalid is not None) and (Yvalid.size != 0):
-            # Mask out pixels whose label we don't care about.
-            Mvalid = np.ones(Yvalid.shape, dtype=bool)
-            for yIgnore in omitLabels:
-                Mvalid[Yvalid==yIgnore] = False
-
-            print "[train]:    Evaluating on validation data (%d pixels)..." % np.sum(Mvalid)
-            Confusion = np.zeros((yMax+1, yMax+1))    # confusion matrix
-                
-            it = emlib.interior_pixel_generator(Yvalid, tileRadius, batchDim[0], mask=Mvalid)
-            for Idx, epochPct in it:
-                # Extract subtiles from validation data set
-                for jj in range(Idx.shape[0]):
-                    yi[jj] = Yvalid[ Idx[jj,0], Idx[jj,1], Idx[jj,2] ]
-                    a = Idx[jj,1] - tileRadius
-                    b = Idx[jj,1] + tileRadius + 1
-                    c = Idx[jj,2] - tileRadius
-                    d = Idx[jj,2] + tileRadius + 1
-                    Xi[jj, 0, :, :] = Xvalid[ Idx[jj,0], a:b, c:d ]
-
-                #----------------------------------------
-                # one forward pass; no backward pass
-                #----------------------------------------
-                solver.net.set_input_arrays(Xi, yi)
-                # XXX: could call preprocess() here?
-                rv = solver.net.forward()
-
-                # extract statistics 
-                Prob = np.squeeze(rv['prob'])       # matrix of estimated probabilities for each object
-                yHat = np.argmax(Prob,1)            # estimated class is highest probability in vector
-                for yTmp in range(yMax+1):          # note: assumes class labels are in {0, 1,..,n_classes-1}
-                    bits = (yi.astype(np.int32) == yTmp)
-                    for jj in range(yMax+1):
-                        Confusion[yTmp,jj] += np.sum(yHat[bits]==jj)
- 
-            print '[train]: Validation results:'
-            print '      %s' % str(Confusion)
-            if yMax == 1:
-                # Assume a binary classification problem where 0 is non-target and 1 is target.
-                #
-                # precision := TP / (TP + FP)
-                # recall    := TP / (TP + FN)
-                #
-                #  Confusion Matrix:
-                #                  yHat=0       yHat=1
-                #     y=0           TN            FP
-                #     y=1           FN            TP
-                #
-                precision = (1.0*Confusion[1,1]) / np.sum(Confusion[:,1])
-                recall = (1.0*Confusion[1,1]) / np.sum(Confusion[1,:])
-                f1Score = (2.0 * precision * recall) / (precision + recall);
-                print '    precision=%0.3f, recall=%0.3f' % (precision, recall)
-                print '    F1=%0.3f' % f1Score
-        else:
-            print '[train]: Not using a validation data set'
-            
-        sys.stdout.flush()
-        # ----- end one epoch -----
-                
- 
-    # complete
-    print "[train]:    all done!"
-    return losses, acc
+        #---------------------------------------- 
+        # one forward pass; no backward pass 
+        #----------------------------------------
+        net.set_input_arrays(Xi, yi)
+        out = net.forward()
 
    
 
@@ -441,6 +307,14 @@ def _load_data(args):
     # Make sure the dimensions look ok before proceeding.
     assert(Xtrain.shape[0] < Xtrain.shape[1])
     assert(Xtrain.shape[0] < Xtrain.shape[2])
+
+    # take a subset of slices (optional)
+    if args.onlySlices:
+        useSlices = eval(args.onlySlices)
+        Xtrain = Xtrain[useSlices,:,:]
+        Ytrain = Ytrain[useSlices,:,:]
+        Xvalid = Xvalid[useSlices,:,:]
+        Yvalid = Yvalid[useSlices,:,:]
 
     print('[train]: training data shape: %s' % str(Xtrain.shape))
     print('[train]: validation data shape: %s' % str(Xvalid.shape))
@@ -520,6 +394,10 @@ if __name__ == "__main__":
     for name, blobs in solver.net.params.iteritems():
         for bIdx, b in enumerate(blobs):
             print("%s[%d] : %s" % (name, bIdx, b.data.shape))
+    for ii,layer in enumerate(solver.net.layers):
+        print("layer %d: %s" % (ii, layer.type))
+        for jj,blob in enumerate(layer.blobs):
+            print("  blob %d has size %s" % (jj, str(blob.data.shape)))
 
 
     #----------------------------------------
